ec4badd5b6dd2cd82e43571802bc97b61448c3e3
Lichao Huang <lhuang38@rrc.ca>
2025-10-02 08:38:01 -0500

    add listener

--- script.js
+++ script.js
@@ -14,4 +14,28 @@ let questionLevelZeroQuestion = "Press the button to start the adventure";
 adventureQuestionID.textContent = questionLevelZeroQuestion;
 
 
+let adventureLevelOneTitle = "Take on adventure missions"
+let questionLevelOneQuestion = `You walk into the Adventurer's Guild,
+your most junior adventurer, so the guild offers you two options.`;
+
+const bottonID = document.getElementById("next-btn")
+const gameContainer = document.getElementById("game-container");
+
+bottonID.addEventListener("click", () => {
+    adventureTitleID.textContent = adventureLevelOneTitle;
+    adventureQuestionID.textContent = questionLevelOneQuestion;
+    const redMission = document.createElement("button");
+    const blueMission = document.createElement("button");
+    redMission.textContent = "Crystal Collection";
+    blueMission.textContent = "Gathering acorns";
+    redMission.id = "redMission"
+    blueMission.id = "blueMission"
+    gameContainer.appendChild(redMission);
+    gameContainer.appendChild(blueMission);
+    bottonID.remove();
+})
+
+
+
+
 
